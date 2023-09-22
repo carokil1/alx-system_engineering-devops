9d56c6eca4826bead44e73d0a9e26508157b7328
Carokil1 <chucholimosr@gmail.com>
2023-09-22 12:42:38 -0700

    wueh5

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#This code creates a file in /tmp
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
+
