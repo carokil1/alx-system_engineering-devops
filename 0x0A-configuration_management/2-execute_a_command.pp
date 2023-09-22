d220fbdce4d90df88a701a27d02847749f522ae2
Carokil1 <chucholimosr@gmail.com>
2023-09-22 14:22:46 -0700

    nooooooo

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
-#This code kills a prcoess and works with the killmenow file provide
-exec { 'killmenow':
-  command  => 'usr/bin/pkill killmenow',
-  provider => 'shell',
-  returns  => [0, 1],
-}
+# kill process killmenow
 
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
+}
